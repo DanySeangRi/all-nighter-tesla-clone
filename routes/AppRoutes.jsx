a94ae9e3b833a0c81826e03ca4b9289ef28265d4
ka-te-vey <oscar19092019@gmail.com>
2025-12-16 18:32:22 +0700

    feature(vdoshowcase): change route path

--- routes/AppRoutes.jsx
+++ routes/AppRoutes.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from '../pages/Home';
+import VideoShowcase from '../src/components/learn-more/VideoShowcase';
 
 export default function AppRoutes() {
   return (
-    <div>
-      
-    </div>
-  )
+    <Routes>
+      {/* <Route path="/" element={<Home />} /> */}
+      <Route path="/video" element={<VideoShowcase />} />
+    </Routes>
+  );
 }
