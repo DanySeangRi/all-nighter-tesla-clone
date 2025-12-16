387dd6888c2f8b312dd4d01292db3c018f26b439
Sochary Vanda <vsochary@gmail.com>
2025-12-16 17:01:53 +0700

    feature(shop-page): add picture

---
+++ src/components/shop/ShopHome.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ModelY from "../../assets/images/shop/tesla-shop-marketing/ModelY.avif";
+
+const ShopHome = () => {
+  return (
+    <div>
+      <img src={ModelY} alt="Tesla Model Y" />
+    </div>
+  );
+};
+
+export default ShopHome;
+
+
+ 
