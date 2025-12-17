02c66073363b0394fa7611a64cd05dcbfc949769
Ponhvorntey Choub <cponhvorntey@gmail.com>
2025-12-17 22:40:09 +0700

    feature(footer): update Home.jsx and Learn.jsx to default

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -3,30 +3,7 @@ import Footer from "/src/components/layout/Footer"
 export default function Home() {
   return (
   <div>
-      
-
-
-
-
-
-
-
-
-
-
-
- {/* FOOTER START */}
-      <div className="min-h-screen flex flex-col">
-      
-      <main className="flex-grow">
-       
-      </main>
-
-      <Footer />
-    </div>
-{/* FOOTER END */}
-  
-  
+    
 </div>
   
 
--- pages/LearnMore.jsx
+++ pages/LearnMore.jsx
@@ -3,31 +3,7 @@ import Footer from "/src/components/layout/Footer"
 export default function LearnMore() {
   return (
     <div>
-      
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-      {/* FOOTER START */}
-            <div className="min-h-screen flex flex-col">
-            
-            <main className="flex-grow">
-             
-            </main>
-      
-            <Footer />
-          </div>
-      {/* FOOTER END */}
     </div>
   )
 }
