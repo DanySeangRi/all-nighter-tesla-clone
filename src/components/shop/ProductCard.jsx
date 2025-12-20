bfa9902bdfdcd8be9c858fd437dc1ff9c487396b
San Tol <tolsan42@gmail.com>
2025-12-21 06:42:48 +0700

    feature(shop-page): modified ProductCard.jsx

--- src/components/shop/ProductCard.jsx
+++ src/components/shop/ProductCard.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-
-export default function ProductCard() {
+import ProductImage from "./ProductImage";
+const ProductCard = ({ product }) => {
   return (
-    <div>
-      
+    <div className=" flex flex-col">
+      <ProductImage
+        src={product.image}
+        hoverSrc={product.imageHover}
+        alt={product.name}
+      />
+      <div className=" mt-4 space-y-0.5 font-bold">
+        <h3 className=" text-sm line-clamp-2 hover:text-red-500">
+          {product.name}
+        </h3>
+        <p className=" text-sm font-semibold">${product.price}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default ProductCard;
