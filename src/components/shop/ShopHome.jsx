b310c4168198c7f163354bcc3d4b03400d1fc2e6
Sochary Vanda <vsochary@gmail.com>
2025-12-20 15:08:06 +0700

    feature(shop-page_home-page): I changed the font-size and font-weight as suggested as well as adding the images for mobile breakpoints

--- src/components/shop/ShopHome.jsx
+++ src/components/shop/ShopHome.jsx
@@ -1,15 +1,109 @@
 import React from 'react'
 import ModelY from "../../assets/images/shop/tesla-shop-marketing/ModelY.avif";
 
-const ShopHome = () => {
+export function ShopHome({ desktopImage, alt, name, textColor,mobileImage }) {
   return (
-    <div>
-      <img src={ModelY} alt="Tesla Model Y" />
+    <div className="relative h-screen w-full">
+      <picture>
+        {/* Desktop */}
+        <source media="(min-width: 640px)" srcSet={desktopImage} />
+
+        {/* Mobile (fallback) */}
+        <img
+          src={mobileImage}
+          alt={alt}
+          className="h-full w-full object-cover"
+        />
+      </picture>
+
+      <div className="absolute bottom-19 left-0 right-0 z-10 flex flex-col items-center gap-5 px-4 ">
+        <h2 className= {`text-[24px] md:text-[40px] font-normal   text-center ${textColor}   font-medium md:font-medium md:mb-3 mb-3`}  >
+          {name}
+        </h2>
+
+        <button className=" bg-white rounded-sm text-[14px] font-normal px-10 py-2 w-full max-w-md md:w-[283px]  text-black font-bold">
+          Shop now
+        </button>
+      </div>
     </div>
   );
-};
+}
+// this is for rendering all the images
 
-export default ShopHome;
+export function Rendering() {
+  const forRendering = [
+    {
+      desktopImage : "src/assets/images/shop/tesla-shop-marketing/ModelY.avif",
+      mobileImage: "src/assets/images/shop/tesla-shop-marketing/modelymobile.avif",
+      er: "Model Y Roof Rack",
+      title: "Model Y Roof Rack",
+      textColor: "text-black"
+    },
+    {
+      desktopImage : "src/assets/images/shop/tesla-shop-marketing/Model_Y_2800x1300.avif",
+      mobileImage:"src/assets/images/shop/tesla-shop-marketing/DSCF8694-1_800x1700.avif",
+      er: "Model Y Accessories",
+      title: "Model Y Accesories",
+      textColor: "text-white"
+    },
+    {
+      desktopImage : "src/assets/images/shop/tesla-shop-marketing/M3H_desktop-2800x1300.avif",
+      mobileImage:"src/assets/images/shop/tesla-shop-marketing/mobile-app-800x1700.avif",
+      er: "Model 3 Accesories",
+      title: "Model 3 Accesories",
+      textColor: "text-black"
+    },
+    {
+      desktopImage : "src/assets/images/shop/tesla-shop-marketing/MS_web.avif",
+      mobileImage:"src/assets/images/shop/tesla-shop-marketing/MS_mobile.avif",
+      er: "Model S Accesories",
+      title: "Model S Accesories",
+      textColor: "text-white"
+    },
+    {
+      desktopImage : "src/assets/images/shop/tesla-shop-marketing/MX_web.avif",
+      mobileImage:"src/assets/images/shop/tesla-shop-marketing/MX_mobile.avif",
+      er: "Model X Accesories",
+      title: "Model X Accesories",
+      textColor: "text-white"
+    },
+    {
+      desktopImage : "src/assets/images/shop/tesla-shop-marketing/Cybertruck2.avif",
+      mobileImage:"src/assets/images/shop/tesla-shop-marketing/4Z65LglsywtlS42_mobile.avif",
+      er: "Cybertruck Accesories",
+      title: "Cybertruck Accesories",
+      textColor: "text-white"
+    },
+    {
+      desktopImage: "src/assets/images/shop/tesla-shop-marketing/charging2.avif",
+      mobileImage:"src/assets/images/shop/tesla-shop-marketing/fvW6NvXGLVYM7kco63br_mobile.avif",
+      er: "Charging",
+      title: "Charging",
+      textColor: "text-white"
+    },
+    {
+      desktopImage: "src/assets/images/shop/tesla-shop-marketing/Lifestyle.avif",
+      mobileImage:"src/assets/images/shop/tesla-shop-marketing/WirelessCharger1800x1700.avif",
+      er: "Lifestyle",
+      title: "Lifestyle",
+      textColor: "text-black"
+    },
+  ];
 
-
- 
+  return (
+    <>
+      <ul>
+        {forRendering.map((render) => {
+          return (
+            <ShopHome 
+            desktopImage={render.desktopImage}
+            mobileImage={render.mobileImage} 
+            alt={render.er} 
+            name={render.title} 
+            textColor={render.textColor}/>
+          );
+        })}
+      </ul>
+    </>
+  );
+}
