82868ce575bc9c67fb5db6a59861c51b6d419861
ka-te-vey <oscar19092019@gmail.com>
2025-12-20 16:52:12 +0700

    feature(vd0-showcase): change some files

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import HeroSection from '../src/components/homepage/HeroSection';
-import Production from '../src/components/homepage/Production';
+
 
 export default function Home() {
   return (
--- routes/AppRoutes.jsx
+++ routes/AppRoutes.jsx
@@ -1,13 +1,9 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Home from '../pages/Home';
-import VideoShowcase from '../src/components/learn-more/VideoShowcase';
+import React from 'react'
 
 export default function AppRoutes() {
   return (
-    <Routes>
-      {/* <Route path="/" element={<Home />} /> */}
-      <Route path="/video" element={<VideoShowcase />} />
-    </Routes>
-  );
+    <div>
+      
+    </div>
+  )
 }
