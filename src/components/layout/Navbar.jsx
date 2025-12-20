9e5ac5fbe42a97a6030905d3b08b262882ed1b8f
Chhourn-CryMunyvann <kimhak.prome@gmail.com>
2025-12-21 04:50:20 +0700

    feature(navbar_responsive):add menuitem in mobile menu

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import { IoHelpCircleOutline, IoClose } from "react-icons/io5";
+import { IoHelpCircleOutline, IoClose, IoMenuOutline } from "react-icons/io5";
 import { SlGlobe } from "react-icons/sl";
 import { HiOutlineUserCircle } from "react-icons/hi2";
-
+import { IoIosArrowForward } from "react-icons/io";
 import MegaMenu from "./MegaMenu";
 
 export default function Navbar() {
@@ -10,14 +10,21 @@ export default function Navbar() {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   const menuItems = ["Vehicles", "Energy", "Charging", "Discover", "Shop"];
-  const icons = [IoHelpCircleOutline, CiGlobe, HiOutlineUserCircle];
+  const mobileMenuItems = [...menuItems, "Support"];
+
+  const handleMouseLeave = () => {
+    setActiveMenu(null);
+  };
   return (
-  
-    <div className="relative" onMouseLeave={() => setActiveMenu(null)}>
+    <div className="relative">
       {/* NAVBAR */}
       <nav className="fixed top-0 z-50 w-full bg-white">
-        <div className="mx-auto flex h-14 max-w-360 items-center justify-between px-4 lg:px-0 ">         
-          <div className="font-['Tesla'] text-xl uppercase tracking-[0.5em] text-[#282c35]">
+        <div className="mx-auto flex h-14 max-w-360 items-center justify-between px-4 lg:px-0">
+          <div
+            className={`font-['Tesla'] text-xl uppercase tracking-[0.5em] text-[#282c35] ${
+              isMobileMenuOpen ? "hidden" : "block"
+            } lg:block`}
+          >
             Tesla
           </div>
 
@@ -27,6 +34,7 @@ export default function Navbar() {
               <li
                 key={item}
                 onMouseEnter={() => setActiveMenu(item)}
+                onMouseLeave={handleMouseLeave}
                 className="cursor-pointer rounded-sm px-4 py-2 hover:bg-black/5  transition"
               >
                 {item}
@@ -42,7 +50,7 @@ export default function Navbar() {
           </div>
 
           {/* Mobile Menu Toggle Button */}
-          <div className="lg:hidden">
+          <div className={`lg:hidden ${isMobileMenuOpen ? "ml-auto" : ""}`}>
             <button
               onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
               className="rounded-sm px-4 py-2 text-[14px] font-medium tracking-[1px] hover:bg-black/5 transition bg-gray-100"
@@ -55,31 +63,52 @@ export default function Navbar() {
 
       {/* MOBILE MENU */}
       <div
-        className={`fixed inset-0 z-40 transform bg-white transition-transform duration-300 ease-in-out ${
-          isMobileMenuOpen ? "translate-x-0" : "translate-x-full"
-        } lg:hidden pt-14`}
+        className={`fixed inset-0 z-40 bg-white p-4 transition-opacity duration-600 ease-in-out ${
+          isMobileMenuOpen
+            ? "opacity-100 visible, mt-10"
+            : "opacity-0 invisible"
+        } lg:hidden`}
       >
-        <div className="flex justify-end px-2 py-6">
-          <button onClick={() => setIsMobileMenuOpen(false)}>
-            <IoClose className="text-[26px]" />
-          </button>
-        </div>
-        <ul className="flex flex-col items-center space-y-4 text-xl font-medium">
-          {menuItems.map((item) => (
+        <ul className="flex flex-col items-start ">
+          {mobileMenuItems.map((item) => (
             <li
               key={item}
-              className="cursor-pointer rounded-sm px-4 py-2 hover:bg-black/5 bg-gray-100 transition"
+              className="flex items-center justify-between text-[#171A20] hover:bg-gray-100 rounded-lg w-full px-4 py-4 mt-2 cursor-pointer"
             >
-              {item}
+              <span>{item}</span>
+              <IoIosArrowForward className="text-[18px] text-gray-400" />
             </li>
           ))}
         </ul>
+        {/* Mobile Icons */}
+        <div className="mt-6 space-y-2">
+          {/* Language */}
+          <div className="flex items-center justify-between rounded-lg  px-4 py-4 cursor-pointer hover:bg-gray-100 transition">
+            <div className="flex items-center space-x-4">
+              <SlGlobe className="text-[20px]" />
+              <div className="flex flex-col">
+                <span className="text-[16px] font-medium">United States</span>
+                <span className="text-[14px] text-gray-500">English</span>
+              </div>
+            </div>
+            <IoIosArrowForward className="text-[18px] text-gray-400" />
+          </div>
+
+          {/* Account */}
+          <div className="flex items-center justify-between px-4 py-4 rounded-lg cursor-pointer hover:bg-gray-100 transition">
+            <div className="flex items-center space-x-4">
+              <HiOutlineUserCircle className="text-[22px]" />
+              <span className="text-[16px] font-medium">Account</span>
+            </div>
+            <IoIosArrowForward className="text-[18px] text-gray-400" />
+          </div>
+        </div>
       </div>
 
       {/* DROPDOWNS */}
       {!isMobileMenuOpen && (
         <>
-      <MegaMenu activeMenu={activeMenu} open={activeMenu !== null} />
+          <MegaMenu activeMenu={activeMenu} open={activeMenu !== null} />
         </>
       )}
       {/* Later:
