516fe21311412e4197d8885f55674e321390292b
Chamroeurn Kimseang <chamroeurnkimseang@gmail.com>
2025-12-21 04:54:05 +0700

    feature(home-page):change Homwshowcasedata to powerSupplyData

--- src/components/homepage/PowerSupply.jsx
+++ src/components/homepage/PowerSupply.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import HomeShowcaseCard from './HomeShowcaseCard'
-import HomeShowcaseData from './data'
+import powerSupplyData from './data'
 import { Home } from 'lucide-react'
 import NavigationButtons from '../ui/NavigationButtons'
 import SlideIndicator from '../ui/SlideIndicator'
 const PowerSupply = () => {
   return (
     <div className='flex w-1000 px-12 gap-6 relative'>
-      {HomeShowcaseData.map((item,index)=>(
+      {powerSupplyData.map((item,index)=>(
         <HomeShowcaseCard
         key={index}
         mobileImage={item.imageMobile}
