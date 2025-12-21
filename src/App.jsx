8037c1b9658e27e828bc4174b8ba8b67f92631ac
Chhourn-CryMunyvann <kimhak.prome@gmail.com>
2025-12-21 15:22:29 +0700

    feature(navbar_responsive):create shopnavbar file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Navbar from "./components/layout/Navbar";
+import ShopNavbar from "./components/layout/ShopNavbar";
 
 export default function App() {
   return (
     <>
-      <Navbar />
+     <ShopNavbar/>
     </>
   );
 }
---
+++ src/components/layout/ShopNavbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ShopNavbar() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
