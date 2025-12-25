2fc82d1a3c38f682977205b8359162f846a041cd
Chamroeurn Kimseang <chamroeurnkimseang@gmail.com>
2025-12-25 11:27:06 +0700

    hotfix(shop-page): change images sourse to imported images in shop page hero section

--- src/components/shop/ShopHomePage.jsx
+++ src/components/shop/ShopHomePage.jsx
@@ -4,18 +4,19 @@ import ShopHome from "./ShopHome.jsx";
 import {products} from "./data/products.js";
 import ApparelSection from "./ApparelSection.jsx";
 import ProductGrid from "./ProductGrid.jsx";
-
+import modelYDesktop from "../../assets/images/shop/tesla-shop-marketing/ModelY.avif";
+import modelYMobile from "../../assets/images/shop/tesla-shop-marketing/modelymobile.avif";
 
 const ShopHomePage = () => {
   return (
     <>
-      <ShopHome
-        desktopImage="src/assets/images/shop/tesla-shop-marketing/ModelY.avif"
-        mobileImage="src/assets/images/shop/tesla-shop-marketing/modelymobile.avif"
-        alt="Model Y Roof Rack"
-        title="Model Y Roof Rack"
-        textColor="text-black"
-      />
+        <ShopHome
+          desktopImage={modelYDesktop}
+          mobileImage={modelYMobile}
+          alt="Model Y Roof Rack"
+          title="Model Y Roof Rack"
+          textColor="text-black"
+        />
       <BestSellers products={products} />
 
       <ul>
